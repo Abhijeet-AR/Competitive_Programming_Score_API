db5cb1b34e5df319d7d2379a31578ec2aff07142
Abhijeet Reddy <abhijeet_abhi@live.co.uk>
2020-02-08 17:44:07 +0530

    Added Codeforces and SPOJ API

--- details_soup.py
+++ details_soup.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 
-import requests
+import requests, json
 
 
 class UsernameError(Exception):
@@ -49,7 +49,7 @@ class UserData:
                               'global rank': rating_table_rows[2].a.hx.text,
                               'country rank': rating_table_rows[3].a.hx.text}
 
-            cook_off = {'name': 'Cook-off','rating': rating_table_rows[5].text,
+            cook_off = {'name': 'Cook-off', 'rating': rating_table_rows[5].text,
                         'global rank': rating_table_rows[6].a.hx.text,
                         'country rank': rating_table_rows[7].a.hx.text}
 
@@ -63,6 +63,73 @@ class UserData:
                    'contests': contests_details_get()}
 
         '''Can add latest contests details'''
+
+        return details
+
+    def __codeforces(self):
+        url = 'https://codeforces.com/api/user.info?handles={}'.format(self.__username)
+
+        page = requests.get(url)
+
+        if page.status_code != 200:
+            raise UsernameError('User not Found')
+
+        details_api = page.json()
+
+        if details_api['status'] != 'OK':
+            raise UsernameError('User not Found')
+
+        details_api = details_api['result'][0]
+
+        details = {'status': 'Success', 'username': self.__username, 'platform': 'Codeforces',
+                   'rating': details_api['rating'], 'max rating': details_api['maxRating'],
+                   'rank': details_api['rank'], 'max rank': details_api['maxRank']}
+
+        return details
+
+    def __spoj(self):
+        url = 'https://www.spoj.com/users/{}/'.format(self.__username)
+
+        page = requests.get(url)
+
+        soup = BeautifulSoup(page.text, 'html.parser')
+        details_container = soup.find_all('p')
+
+        points = details_container[2].text.split()[3][1:]
+        rank = details_container[2].text.split()[2][1:]
+        join_date = details_container[1].text.split()[1] + ' ' + details_container[1].text.split()[2]
+        institute = ' '.join(details_container[3].text.split()[1:])
+
+        def get_solved_problems():
+            table = soup.find('table', class_='table table-condensed')
+
+            rows = table.findChildren('td')
+
+            solved_problems = []
+            for row in rows:
+                if row.a.text:
+                    solved_problems.append(row.a.text)
+
+            return solved_problems
+
+        def get_todo():
+            try:
+                table = soup.find_all('table', class_='table')[1]
+            except:
+                return None
+
+            rows = table.findChildren('td')
+
+            todo_problems = []
+            for row in rows:
+                if row.a.text:
+                    todo_problems.append(row.a.text)
+
+            return todo_problems
+
+        details = {'status': 'Success', 'username': self.__username, 'platform': 'SPOJ',
+                   'points': points, 'rank': rank, 'solved': get_solved_problems(),
+                   'todo': get_todo(), 'join data': join_date, 'institute': institute}
 
         return details
 
@@ -86,11 +153,20 @@ class UserData:
         return details
 
     def get_details(self, platform):
-        if platform == 'interviewbit':
-            return self.__interviewbit()
-
         if platform == 'codechef':
             return self.__codechef()
+
+        if platform == 'codeforces':
+            return self.__codeforces()
+
+        if platform == 'spoj':
+            try:
+                return self.__spoj()
+            except AttributeError:
+                raise UsernameError('User not Found')
+
+        if platform == 'interviewbit':
+            return self.__interviewbit()
 
         raise PlatformError('Platform not Found')
 
@@ -98,6 +174,6 @@ class UserData:
 if __name__ == '__main__':
     ud = UserData('abhijeet_ar')
 
-    ans = ud.get_details('codechef')
+    ans = ud.get_details('spoj')
 
     print(ans)
