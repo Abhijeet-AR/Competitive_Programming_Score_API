c330a93049c8bf38cb8d614088503e8b79c37899
Abhijeet Reddy <abhijeet_abhi@live.co.uk>
2020-02-27 17:16:00 +0530

    added CORS

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 from details_soup import UserData, UsernameError, PlatformError
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
